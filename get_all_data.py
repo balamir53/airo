0030c93c199c49c37a16eff35d49745889bf967e
balamir53 <balamir53@yahoo.com>
2024-02-23 10:06:22 +0300

    all data in one file

---
+++ get_all_data.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+from pathlib import Path
+
+TOTAL_ROW_NUMBER = 569839590
+
+user = 'lydiausr'
+password = 'lydia2024!'
+host = '10.255.0.230'
+port = '5432'
+db = 'lydia'
+connection_string = f"postgresql://{user}:{password}@{host}:{port}/{db}"
+engine = create_engine(connection_string)
+conn = engine.connect().execution_options(
+        stream_results=True)
+# query = "SELECT classavailable, deptime, arrtime, brand, price FROM airo.daily_price"
+offset = 0
+offset = 0
+chunksize = 1_000_000
+# query = f'select classavailable, deptime, arrtime, brand, price FROM airo.daily_price LIMIT {chunksize} OFFSET {offset}'
+query = f'select substring(addedtodownloadqueue ,0,11) addedtodownloadqueue, deptime, recordroute, flightno, min(price) price FROM airo.newview group by deptime, flightno, substring(addedtodownloadqueue ,0,11), recordroute'
+count = 0
+folder_path = f'{Path.home()}\\airo\\data'
+
+# for chunk_dataframe in pd.read_sql_query(query , conn, parse_dates = ['deptime', 'arrtime'], dtype={'price': np.int32}, chunksize=5000000):
+# for chunk_dataframe in pd.read_sql_query(query , conn, parse_dates = ['deptime', 'arrtime'], dtype={'price': np.int32}):
+# for i in range(TOTAL_ROW_NUMBER//chunksize):
+        # data = pd.read_sql_query(query , conn, parse_dates = ['deptime', 'arrtime'], dtype={'price': np.int32})
+data = pd.read_sql_query(query , conn, dtype={'price': np.int32})
+file_path = folder_path + '/groupby-all%s.pkl' % (count)
+data.to_pickle(file_path)
+print(f'{file_path} has been written with offset:{offset} with chunksize: {chunksize}')
+# count += 1
+# offset += chunksize
+        # query = f'select substring(addedtodownloadqueue ,0,11) addedtodownloadqueue, deptime, recordroute, flightno, min(price) price FROM airo.newview group by deptime, flightno, substring(addedtodownloadqueue ,0,11), recordroute limit {chunksize} offset {offset}'
--- test_load.py
+++ test_load.py
@@ -1,6 +1,6 @@
 import pickle
 import pandas
-with open (r'C:\Users\yusuf\airo\data\dataset0.pkl','rb') as file:
+with open (r'C:\Users\yusuf\airo\data\groupby-all0.pkl','rb') as file:
     # data = pickle.load(file)
     data = pandas.read_pickle(file)
-print(data.info())
+print(data.tail())
