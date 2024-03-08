213627e97088f39067b8bc1344996b17f9284277
ebron01 <emreboran@hacettepe.edu.tr>
2024-03-08 17:12:41 +0300

    added chatbot langflow

---
+++ chatbot.py
@@ -0,0 +1,35 @@
+import pickle
+from pathlib import Path
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
+
+from langchain.agents.agent_types import AgentType
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAI
+
+
+
+myPath = f'{Path.home()}/airo/data'
+onlyfiles = [f for f in listdir(myPath) if isfile(join(myPath,f))]
+for f in onlyfiles:
+    #should we concat them one by one
+    #will this consume more memory
+    if  first:
+        with open (join(myPath,f),'rb') as file:
+            data1 = pickle.load(file)
+            first = False
+    else:
+        with open (join(myPath,f),'rb') as file:
+            data1 = pd.concat([data1,pickle.load(file)])
+            
+agent = create_pandas_dataframe_agent(OpenAI(temperature=0), data1, verbose=True)
+
+agent = create_pandas_dataframe_agent(
+    ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613"),
+    data1,
+    verbose=True,
+    agent_type=AgentType.OPENAI_FUNCTIONS,
+)
+agent.run("how many rows are there?")
